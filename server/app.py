5e1c3a6fe0d17cad960b61ff21ed1b3f5d054c04
Ubuntu <ubuntu@ip-172-31-1-47.us-west-1.compute.internal>
2020-09-21 15:27:02 +0000

    basic server configuration

--- server/app.py
+++ server/app.py
@@ -1,8 +1,15 @@
 from flask import Flask
-from flask import send_file
+#from flask import send_file
 
 app = Flask(__name__)
 
-@app.route('/predict')
-def predict_image():
-    return send_file("./prediction.png", mimetype='image/png')
+#@app.route('/predict')
+#def predict_image():
+#    return send_file("./prediction.png", mimetype='image/png')
+
+@app.route('/')
+def hello_world():
+    return "Hello from Flask!"
+
+if __name__ == '__main__':
+    app.run()
