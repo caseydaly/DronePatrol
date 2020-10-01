e09f88ae930dd6f6ace09694cdd43ef45347dc99
Ubuntu <ubuntu@ip-172-31-247-92.us-west-1.compute.internal>
2020-10-01 22:06:46 +0000

    removed unnecessary files

--- server/main.py
+++
@@ -1,7 +0,0 @@
-
-
-if __name__ == '__main__':
-    #model will live on server
-    parser = argparse.ArgumentParser(description='Predict sharks from videos or YouTube live streams.')
-    parser.add_argument('input_data', help="Either the URL of the YouTube live stream, or a local file path of an mp4 video.")
-    args = parser.parse_args()
