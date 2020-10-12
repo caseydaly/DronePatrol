25b04033b76ea6517446cd4666407abdf8571bc9
caseydaly <caseydaly31@gmail.com>
2020-10-12 15:42:05 -0700

    added location selector, need to style better

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MapGL from 'react-map-gl';
+import LocationSelector from './LocationSelector';
 
 
 export default class Sidebar extends React.Component {
@@ -18,9 +19,11 @@ export default class Sidebar extends React.Component {
 
         return (
 
-            <div style={{position: "fixed", width: "33%", height: "86%", backgroundColor: "white", marginLeft: "2.6%", marginTop: "2.9%", paddingLeft: "1.65%", paddingTop: "2%", borderRadius: 25}}>
-                <h1 style={{fontSize: "24px", color: "#0075DF", fontWeight: 700, marginBottom: "-3%"}}>Drone Patrol</h1>
-                <p style={{fontSize: "16px"}}> Locate sharks in your area. </p>
+            <div style={{position: "fixed", width: "33%", height: "86%", backgroundColor: "white", marginLeft: "2.6%", marginTop: "2.9%", paddingLeft: "1.65%", paddingTop: "35px", borderRadius: 25}}>
+                <h1 style={{fontSize: "24px", color: "#0075DF", fontWeight: 700, marginBottom: "0px", marginTop: "0px"}}>Drone Patrol</h1>
+                <p style={{fontSize: "16px", marginTop: "8px"}}> Locate sharks in your area. </p>
+                <p style={{fontWeight: 500, marginTop: "30px"}}>Choose Location</p>
+                <LocationSelector />
             </div>
         );
     }
