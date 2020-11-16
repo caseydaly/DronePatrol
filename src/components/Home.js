95569c468abbfb06f46803bf7510717d5e998d35
caseydaly <caseydaly31@gmail.com>
2020-11-16 09:28:53 -0800

    started final 'report sighting' display

--- src/components/Home.js
+++ src/components/Home.js
@@ -42,13 +42,12 @@ export default class HomeScreen extends React.Component {
                 pitch: 0
             },
             popup: null,
-            reportSightingLocation: false,
-            reportSightingFinish: false,
             spots: [],
             userCurrentLat: null,
             userCurrentLon: null,
             showSightings: true,
-            addSightingMarker: null
+            addSightingMarker: null,
+            currentSidebar: null
         };
         this._renderIcon.bind(this);
         this._iconClick.bind(this);
@@ -57,26 +56,30 @@ export default class HomeScreen extends React.Component {
     async getSpots() {
         const response = await fetch("http://ec2-50-18-14-124.us-west-1.compute.amazonaws.com/api/spots");
         const spots = await response.json();
-        this.setState({ spots: spots })
+        this.state.spots = spots;
+
     }
 
     async componentDidMount(props) {
-        navigator.geolocation.getCurrentPosition(
-            position => {
-                const { latitude, longitude } = position.coords;
-
-                this.setState({
-                    viewport: { latitude: latitude, longitude: longitude, zoom: 5, bearing: 0, pitch: 0 },
-                    loading: false,
-                    userCurrentLat: latitude,
-                    userCurrentLon: longitude
-                });
-            },
-            () => {
-                this.setState({ loading: false });
-            }
-        );
-        this.getSpots();
+        if (this.state.loading) {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    const { latitude, longitude } = position.coords;
+    
+                    this.setState({
+                        viewport: { latitude: latitude, longitude: longitude, zoom: 5, bearing: 0, pitch: 0 },
+                        loading: false,
+                        userCurrentLat: latitude,
+                        userCurrentLon: longitude
+                    });
+                },
+                () => {
+                    this.state.loading = false
+                }
+            );
+            this.getSpots();
+            this.setState({currentSidebar: <Sidebar spots={this.state.spots} opacity={this.getOpacity()} onChange={this.changeSearchArea.bind(this)} reportSightingHandler={this.reportSightingLocationHandler.bind(this)} /> })
+        }
 
     }
 
@@ -110,20 +113,25 @@ export default class HomeScreen extends React.Component {
     }
 
     changeSearchArea(location, zoomFactor) {
-        this.setState({viewport: {latitude: location.lat, longitude: location.lon, zoom: zoomFactor, bearing: 0, pitch: 0}});
+        this.setState({ viewport: { latitude: location.lat, longitude: location.lon, zoom: zoomFactor, bearing: 0, pitch: 0 } });
     }
 
     reportSightingLocationHandler() {
         console.log("reportSightingLocationHandler - Home")
-        this.setState({reportSightingLocation: true, reportSightingFinish: false, showSightings: false});
-    }
-
-    reportSightingFinishHandler() {
-        this.setState({reportSightingLocation: false, reportSightingFinish: true, showSightings: false});
+        this.setState({
+            currentSidebar:
+                <ReportLocation
+                    onChangeLocation={this.changeSearchArea.bind(this)}
+                    zoomOnCurrentLocation={this.zoomOnCurrentLocation.bind(this)}
+                    onNavBack={this.finalizeReportSighting.bind(this)}
+                    spots={this.state.spots}
+                />,
+            showSightings: false
+        });
     }
 
     finalizeReportSighting() {
-        this.setState({reportSightingLocation: false, reportSightingFinish: false, showSightings: true});
+        this.setState({ reportSightingLocation: false, reportSightingFinish: false, showSightings: true });
     }
 
     async zoomOnCurrentLocation() {
@@ -147,7 +155,9 @@ export default class HomeScreen extends React.Component {
                     captureClick={true}
                 >
                     <img src={SharkIconFilledRed} />
-                </Marker>
+                </Marker>,
+            currentSidebar: <ReportFinish />,
+            showSightings: false
         });
     }
 
@@ -156,36 +166,30 @@ export default class HomeScreen extends React.Component {
         if (this.state.loading) {
             return null;
         }
-        
+
         return (
 
             <div style={{ width: "100%", height: "100%" }}>
-                <div style={{ position: "fixed", opacity: this.getOpacity()}}>
+                <div style={{ position: "fixed", opacity: this.getOpacity() }}>
                     <MapGL
                         {...this.state.viewport}
-                        style={{position: "fixed", opacity: this.getOpacity()}}
+                        style={{ position: "fixed", opacity: this.getOpacity() }}
                         width="100vw"
                         height="100vh"
                         mapStyle="mapbox://styles/mapbox/streets-v11"
                         onViewportChange={viewport => this.setState({ viewport })}
                         mapboxApiAccessToken={MAPBOX_TOKEN}
-                        onClick={this.state.reportSightingLocation ? this.handleReportSightingClick.bind(this) : undefined}
+                        onClick={this.state.currentSidebar && this.state.currentSidebar.type == ReportLocation ? this.handleReportSightingClick.bind(this) : undefined}
                     >
                         {this.state.showSightings && sightings.map(this._renderIcon.bind(this))}
                         {this.state.addSightingMarker}
                     </MapGL>
                 </div>
 
-                {this.state.reportSightingLocation && <ReportLocation onChangeLocation={this.changeSearchArea.bind(this)} zoomOnCurrentLocation={this.zoomOnCurrentLocation.bind(this)} onNavBack={this.finalizeReportSighting.bind(this)} spots={this.state.spots}/>}
-
-                {this.state.reportSightingFinish && <ReportFinish />}
-
-                {!this.state.reportSightingLocation && 
-                !this.state.reportSightingFinish &&
-                <Sidebar spots={this.state.spots} opacity={this.getOpacity()} onChange={this.changeSearchArea.bind(this)} reportSightingHandler={this.reportSightingLocationHandler.bind(this)}/>}
+                {this.state.currentSidebar}
 
                 {this.state.popup &&
-                    <SightingPopup sighting={this.state.popup} handleClose={this.onClose.bind(this)}/>
+                    <SightingPopup sighting={this.state.popup} handleClose={this.onClose.bind(this)} />
                 }
 
             </div>
--- src/components/ReportFinish.js
+++ src/components/ReportFinish.js
@@ -1,19 +1,124 @@
 import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import IconButton from '@material-ui/core/IconButton';
+import { ReactComponent as NavigateBackIcon } from '../assets/NavigateBackIcon.svg';
+import ReportSightingGraphic from '../assets/ReportSightingGraphic.svg';
+import LocationSelector from './LocationSelector';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import Button from './Button';
+import { ReactComponent as CurrentLocationIcon } from '../assets/CurrentLocationIcon.svg';
+import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from "@date-io/date-fns";
+import { ReactComponent as CalendarIcon } from '../assets/CalendarIcon.svg';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 export default class ReportFinish extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            selectedLocation: null
         }
+    }
+
+    onNavBackSelect() {
+        //this.props.onNavBack();
+    }
+
+    onSubmit() {
+
+    }
+
+    onChooseLocation() {
+
     }
 
     render() {
         return (
-            <div>
-
+            <div style={styles.container}>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                    <IconButton
+                        edge="end"
+                        onClick={this.onNavBackSelect.bind(this)}
+                        style={{ marginLeft: -15, color: "#0075DF" }}
+                    >
+                        <NavigateBeforeIcon
+                            fontSize="large"
+                        />
+                    </IconButton>
+                    <h2 style={{ color: "#0075DF", fontWeight: 800 }}>Report a Shark Sighting</h2>
+                </div>
+                <div style={{ display: "flex" }}>
+                    <h4 style={{ margin: 0, fontWeight: 350 }}>Add information about the sighting.</h4>
+                </div>
+                <div style={{marginTop:20}}>
+                    <h4 style={{ margin: 0, fontWeight: 450, marginBottom: 10 }}>Choose a date</h4>
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <KeyboardDatePicker
+                            fullWidth
+                            className={styles.input}
+                            inputVariant="outlined"
+                            margin="normal"
+                            keyboardIcon={
+                                <InputAdornment position="start" variant="standard">
+                                    <SvgIcon component={CalendarIcon} />
+                                </InputAdornment>
+                            }
+                            InputAdornmentProps={{ position: "start" }}
+                            disableToolbar
+                            variant="inline"
+                            format="MM/dd/yyyy"
+                            id="date-picker-inline"
+                            label="Start date"
+                            value={this.state.startDate}
+                            onChange={this.handleStartChange}
+                            KeyboardButtonProps={{
+                                'aria-label': 'change date',
+                                edge: "start"
+                            }}
+                        />
+                    </MuiPickersUtilsProvider>
+                </div>
+                <div style={{ display: "flex", marginTop: 20 }}>
+                    <h4 style={{ margin: 0, fontWeight: 350 }}>Click below or drag and drop an image/video</h4>
+                </div>
+                <div style={{ display: "flex", flexDirection: "row", width: "100%", justifyContent: "space-between", marginTop: 20 }}>
+                    <div style={{display: "flex", width: "45%"}}>
+                        <Button
+                            text="Submit"
+                            onClick={this.onSubmit.bind(this)}
+                        />
+                    </div>
+                    <div style={{display: "flex", width: "45%"}}>
+                        <Button
+                            text="Change Location"
+                            onClick={this.onChooseLocation.bind(this)}
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
+
+}
+
+const styles = {
+    container: {
+        position: "fixed",
+        width: "33%",
+        backgroundColor: "white",
+        marginLeft: "1%",
+        marginTop: "1%",
+        padding: "1.5%",
+        borderRadius: 25,
+        cursor: 'default',
+        zIndex: 10
+    },
+    iconColorPrimary: {
+        color: ""
+    },
+    input: {
+        background: "#F4F7F9",
+        width: "100%"
+    },
 
 }
